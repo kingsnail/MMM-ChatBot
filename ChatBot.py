4c5194264cfd2f7f5923683e4c23e1d8d9f3652b
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-07-02 12:25:26 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -20,5 +20,5 @@ try:
     while True:
         keyword_index = porcupine.process(recorder.read())
         if keyword_index >= 0:
-            print("Detected" + config["picovoice"]["KeywordNames"][keyword_index]
+            print("Detected" + config["picovoice"]["KeywordNames"][keyword_index])
             recorder.stop()
