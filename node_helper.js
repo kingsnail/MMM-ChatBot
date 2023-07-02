2fc680a4be5983723ea6f5a34200266dd991238f
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-07-02 17:54:41 +0300

    Create node_helper.js

---
+++ node_helper.js
@@ -0,0 +1,39 @@
+/* ChatBot
+ * Module: MMM-ChatBot
+ *
+ * By Mark Pearce
+ *
+ */
+const NodeHelper = require('node_helper');
+const request = require('request');
+const spawn = require("child_process").spawn; // Required to be able to make calls to Python code externally
+
+var self; // Used for reference to this module when 'this' is out of scope.
+
+module.exports = NodeHelper.create({
+	
+
+    start: function() {
+        console.log("Starting node_helper for: " + this.name);
+	      self = this;
+    },
+
+    updatedStatus: function(stat){
+        self.sendSocketNotification('STATUS_RESULT', stat);
+    },
+
+    getStatus: function(callback) {    
+	     var devstr = "Test";
+        /* Call the external python module to get the system status  */
+       const pythonStatusProcess = spawn('python',["ChatBbotStatus.py"]);
+	     pythonProcess.stdout.on('data', function (data) { var result = JSON.parse(data.toString());
+							  callback(result);							 
+						        });
+    },
+ 
+    socketNotificationReceived: function(notification, payload) {
+	      if (notification === 'GET_STATUS') {
+	         this.getStatus(this.updatedStatus);
+	      }
+    }
+});
