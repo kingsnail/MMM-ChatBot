e88f8bc383f3672b1d136e2b3aa47ecb30efa8be
Mark Pearce <37291931+kingsnail@users.noreply.github.com>
2023-07-03 19:06:25 +0300

    Update MMM-ChatBot.js

--- MMM-ChatBot.js
+++ MMM-ChatBot.js
@@ -28,23 +28,13 @@ Module.register("MMM-ChatBot", {
         requiresVersion: "2.1.0",
 
         // Set locale.
-	this.Devices = [];
-	this.Presets = [];
+	this.Status = "Unknown";
+	this.LastResult = "No Results.";
 	this.lastclicked = -1;
         this.activeItem = 0;         // <-- starts rotation at item 0 (see Rotation below)
         this.rotateInterval = null;  // <-- sets rotation time (see below)
         this.scheduleUpdate();       // <-- When the module updates (see below)
 	var self = this;
-    },
-
-    compareDevs: function( a, b ) {
-        if ( a.name < b.name ){
-             return -1;
-        }
-        if ( a.name > b.name ){
-             return 1;
-        }
-        return 0;
     },
 	
     getDom: function() {
@@ -56,7 +46,7 @@ Module.register("MMM-ChatBot", {
 
 		// The loading sequence
         if (!this.loaded) {
-            wrapper.innerHTML = "Loading Devices . . !";
+            wrapper.innerHTML = "Checking Status ... !";
             wrapper.classList.add("bright", "light", "small");
             return wrapper;
         }
@@ -70,104 +60,49 @@ Module.register("MMM-ChatBot", {
         }
 
 		// Rotating the data
-        // Display the lights
+        // Display the Status
 	var Devs = this.Devices.devices.sort(this.compareDevs);
 	var Groups = this.Devices.groups.sort(this.compareDevs);
 
 	// Creating the div's for your data items
         var top = document.createElement("div");
         top.classList.add("list-row");
+        var stateTitleSpan = document.createElement("span");
+	stateTitleSpan.classList.add("small", "bright", "status_title");
+	stateTitleSpan.innerHTML = "STATUS: ";
+        var stateValueSpan = document.createElement("span");
+	stateValueSpan.classList.add("small", "bright", "status_value");
+        stateValueSpan.innerHTML = this.Status;
+	top.appendChild(stateTitleSpan);
+	top.appendChild(stateValueSpan);
+	wrapper.appendChild(top);
 	    
-	for(var dev = 0; dev < Devs.length; dev++){	
-		var devrow = document.createElement("div");
-		var statespan = document.createElement("span");
-		var textspan  = document.createElement("span");
-		if (Devs[dev].state === "on"){
-            	    statespan.classList.add("small", "bright", "state_on");
-		} else {
-		    statespan.classList.add("small", "bright", "state_off");
-		}
-		textspan.classList.add("small", "bright", "state");
-		devrow.classList.add("small", "bright", "staterow");
-		
-		statespan.innerHTML = Devs[dev].state.toUpperCase();
-		textspan.innerHTML  = " " + Devs[dev].name + " ID(" + Devs[dev].bulbID + "), B(" + Devs[dev].brightness + "), W(" + Devs[dev].warmth + ")";
-            	
-		const d = Devs[dev].bulbID;
-		const s = Devs[dev].state;
-		statespan.addEventListener ("click", ()=>{this.setDevice(d, s)}, false);
-		
-		devrow.appendChild(statespan);
-		devrow.appendChild(textspan);
-            	wrapper.appendChild(devrow);
-	}
-	
-	//for(var grp = 0; grp < Groups.length; grp++){
-	//        var grprow = document.createElement("div");
-        //        var gstate = document.createElement("span");
-	//	var gtext  = document.createElement("span");
-		
-	//	if (Groups[grp].state === "on"){
-   	//	    gstate.classList.add("small", "bright", "state_on");
+	//for(var dev = 0; dev < Devs.length; dev++){	
+	//	var devrow = document.createElement("div");
+	//	var statespan = document.createElement("span");
+	//	var textspan  = document.createElement("span");
+	//	if (Devs[dev].state === "on"){
+        //  	    statespan.classList.add("small", "bright", "state_on");
 	//	} else {
-	//	    gstate.classList.add("small", "bright", "state_off");
+	//	    statespan.classList.add("small", "bright", "state_off");
 	//	}
-	//	gtext.classList.add("small", "bright", "state");
-	//	grprow.classList.add("small", "bright", "staterow");
+	//	textspan.classList.add("small", "bright", "state");
+	//	devrow.classList.add("small", "bright", "staterow");
 	//	
-	//	gstate.innerHTML = Groups[grp].state.toUpperCase();
-	//	gtext.innerHTML  = " Group: " + Groups[grp].groupID + "(" + Groups[grp].name + ")";
-	//	grprow.appendChild(gstate);
-	//	grprow.appendChild(gtext);
-	//	wrapper.appendChild(grprow);
-	//}
-	
-	var presets = this.config.presets;
-	for(var p = 0; p < presets.length; p++){
-		var prow = document.createElement("div");
-		var pname = document.createElement("span");
-		prow.classList.add("small", "bright");
-                pname.classList.add("small", "bright");
-		pname.innerHTML = presets[p].scene;
-		const pp = p;
-		const pl = presets;
-		pname.addEventListener("click", ()=>{this.setScene(pp, pl)}, false);
-		prow.appendChild(pname);
-		if(this.config.showPresetDetails){
-		    for(l = 0; l < presets[p].lights.length; l++){
-  		        var pdetails = document.createElement("span");
-  		        pdetails.innerHTML = "(" + presets[p].lights[l].id + ", " + presets[p].lights[l].state+ ", " + presets[p].lights[l].brt + ", " + presets[p].lights[l].color +")";
-  		        if(this.lastclicked == p){
-			    pdetails.classList.add("small", "bright", "selected");
-			} else {
-			    pdetails.classList.add("small", "bright", "notselected");
-			}
-	                prow.appendChild(pdetails);
-		    }
-		}
-		wrapper.appendChild(prow);
-	}
-        return wrapper;
-		
+	//	statespan.innerHTML = Devs[dev].state.toUpperCase();
+	//	textspan.innerHTML  = " " + Devs[dev].name + " ID(" + Devs[dev].bulbID + "), B(" + Devs[dev].brightness + "), W(" + Devs[dev].warmth + ")";
+        //  	
+	//	const d = Devs[dev].bulbID;
+	//	const s = Devs[dev].state;
+	//	statespan.addEventListener ("click", ()=>{this.setDevice(d, s)}, false);
+	//	
+	//	devrow.appendChild(statespan);
+	//	devrow.appendChild(textspan);
+        //  	wrapper.appendChild(devrow);
+	//}	
+        return wrapper;	
     }, // <-- closes the getDom function from above
 
-	// this will activate a scene
-    setScene: function(s, plist){
-	    if (s >= 0 && s < plist.length){
-		    console.log("KS-SH: Activate scene " + plist[s].scene);
-		    this.lastclicked = s;
-		    for(var b = 0; b < plist[s].lights.length; b++){
-		        this.setFullDevice(plist[s].lights[b].id,plist[s].lights[b].state, plist[s].lights[b].brt, plist[s].lights[b].color);
-		    }
-	    } else {
-	            console.log("KS-SH: Invalid Scene Id " + s.toString());
-		    }
-    },
-    setFullDevice: function(d, s, b, c){
-        console.log("KS-SH: setFullDevice()");
-        var pl = '{ "device": ' + d.toString() + ', "state": "' + s + '", "bright": ' + b.toString() + ', "color": ' + c.toString() + '}';
-	this.sendSocketNotification('SET_DEVICE_STATE', pl);
-    },
 	// this processes your data
     setDevice: function(d, s) { 
 	if ( s === "on" ) {
@@ -182,27 +117,39 @@ Module.register("MMM-ChatBot", {
 	}
     },
     
-    processDevices: function(data) {
-        this.Devices = data;
-        console.log("KS-SH: Devices Updated");
-	console.log(this.Devices);
+    processStatus: function(data) {
+        this.Status = data;
+        console.log("MMM-ChatBot: Status Updated");
+	console.log(this.Status);
 	this.loaded  = true;
+    },
+	
+    processLastResult: function(data) {
+        this.LastResult = data;
+        console.log("MMM-ChatBot: LastResult Updated");
+	console.log(this.LastResult);
     },
 	
 // this tells module when to update
     scheduleUpdate: function() { 
-	//console.log("KS-SH scheduleUpdate called.");
+	//console.log("MMM-ChatBot scheduleUpdate called.");
         setInterval(() => {
-            this.getDevices();
+            this.getStatus();
         }, this.config.updateInterval);
-        this.getDevices(this.config.initialLoadDelay);
+        this.getStatus(this.config.initialLoadDelay);
     },
 	
     getStatus: function(){
-        console.log("KS-SH: getDevices called...");
+        console.log("MMM-ChatBot: getStatus called...");
+	// Send the GET_STATUS request to node_helper.js for action.
         this.sendSocketNotification('GET_STATUS', this.url);
     },
 
+    getLastResult: function(){
+	console.log("MMM-ChatBot: getLastResult called...");
+        // Send the GET_LAST_RESULT request to node_helper.js for action.
+        this.sendSocketNotification('GET_LAST_RESULT', this.url);    },
+	    
     processSetResponse: function(payload){
         console.log("SET_DEVICE_RESPONSE: " + payload);
     },
@@ -210,11 +157,11 @@ Module.register("MMM-ChatBot", {
 	// this gets data from node_helper
     socketNotificationReceived: function(notification, payload) { 
         if (notification === "STATUS_RESULT") {
-            this.processDevices(payload);
+            this.processStatus(payload);
             this.updateDom(this.config.animationSpeed);
         }
-        if (notification === "SET_DEVICE_RESPONSE") {
-            this.processSetResponse(payload);
+        if (notification === "LAST_RESULT_RESPONSE") {
+            this.processLastResult(payload);
         }
         this.updateDom(this.config.initialLoadDelay);
     },
